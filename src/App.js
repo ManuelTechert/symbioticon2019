13621bafd170f27ddbcd7a8fb289ac4e7ea78a40
h9h <git@h9h.de>
2019-11-04 22:07:59 +0100

    Marvins Style - styled-components installiert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Title} from "./components/globals";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Title>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </Title>
+    </Container>
   );
 }
 
