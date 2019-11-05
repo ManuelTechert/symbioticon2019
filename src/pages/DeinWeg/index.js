64c5f1cc2c546e81d511498e110dfde987e9b1c3
h9h <git@h9h.de>
2019-11-05 15:19:52 +0100

    Dein Weg Einsteiger

--- src/pages/DeinWeg/index.js
+++ src/pages/DeinWeg/index.js
@@ -1,50 +1,98 @@
 import React from 'react'
-import {CardTitle, Container, Title, Card, Larger} from "../../components/globals";
-
-const tipps = [
-    [
-        ['Leere deinen Geist', <div>Bla bla</div>],
-    ],
-    [
-        ['Verbessere deinen Schlaf', <div>Bla bla</div>],
-        ['Schwimmkurs in deiner Nähe', <div>Bla bla</div>],
-    ],
-];
-
-const actions = [
-    ['fitbit'],
-    ['Google Calendar'],
-    ['SleepCycle'],
-    ['Headspace'],
-];
+import {CardTitle, Container, Title, Card, Smaller} from "../../components/globals";
+import {Row, Item} from "../../components/Flex";
+import Plant from './Plant.svg'
+import FitnessTracker from './fitness_tracker.svg'
 
 const DeinWeg = (props) => {
-    const { userId } = props;
+    const {userId} = props;
+    if (userId === 0) return <DeinWegEinsteiger/>;
+    
+    return <DeinWegProfi />;
+};
 
-    const hinweise = tipps[userId];
-    const aufgaben = actions[userId];
+const DeinWegEinsteiger = () => {
 
     return (
         <Container>
             <Title>
                 Dein Weg
             </Title>
-            {hinweise.map(([title, component], i) => (
-                <Card key={i}>
-                    <Larger>{title}</Larger>
-                    {component}
-                </Card>
-            ))}
-            <CardTitle>
-                Verbundene Aufgaben
-            </CardTitle>
-            <Card>
-                {aufgaben.map((aufgabe, i) => (
-                    <div key={i}>
-                        todo
-                    </div>
-                ))}
+            <Row style={{paddingBottom: "10px"}}>
+                <Item>
+                    O
+                </Item>
+                <Item flex={2}>
+                    <div style={{ fontSize: "70%" }}>32 Punkte</div>
+                    <div style={{ fontSize: "100%" }}>1€ Cashback</div>
+                    <div style={{ fontSize: "50%" }}>VOR ZWEI WOCHEN</div>
+                </Item>
+            </Row>
+            <Row style={{paddingBottom: "10px"}}>
+                <Item>
+                    O
+                </Item>
+                <Item flex={2}>
+                    <div style={{ fontSize: "120%" }}>97 Punkte</div>
+                    <div style={{ fontSize: "150%" }}>5€ Cashback</div>
+                    <div style={{ fontSize: "100%" }}>HEUTE</div>
+                </Item>
+            </Row>
+            <Row>
+                <Item>
+                    O
+                </Item>
+                <Item flex={2}>
+                    <div style={{ fontSize: "70%" }}>107 Punkte</div>
+                    <div style={{ fontSize: "100%" }}>7€ Cashback</div>
+                    <div style={{ fontSize: "50%" }}>IN 4 WOCHEN, WENN DU SO WEITREMACHST</div>
+                </Item>
+            </Row>
+            <Title>
+                Bereit für den nächsten Schritt?
+            </Title>
+            <Card style={{backgroundColor: "#067AE4", color: 'white'}}>
+                <Row>
+                    <Item flex={3}>
+                        <CardTitle>
+                            Wasser trinken (empfohlen)
+                        </CardTitle>
+                        <Smaller>
+                            Trinke gleich nach dem Aufstehen ein Glas Wasser, um Deinen Körper in Schwung zu bringen
+                        </Smaller>
+                    </Item>
+                    <Item>
+                        <img src={Plant} alt="Plant" />
+                    </Item>
+                </Row>
             </Card>
+            <Card>
+                <Row>
+                    <Item flex={3}>
+                        <CardTitle>
+                            Verbinde weitere Services
+                        </CardTitle>
+                        <Smaller>
+                            Je mehr Services und Dienste du verbindest, desto mehr Insights können wir dir liefern.
+                        </Smaller>
+                    </Item>
+                    <Item>
+                        <img src={FitnessTracker} alt="Fitness Tracker" />
+                    </Item>
+                </Row>
+            </Card>
+        </Container>
+    )
+};
+
+const DeinWegProfi = () => {
+
+    return (
+        <Container>
+            <Title>
+                Dein Weg
+            </Title>
+            Profi tbd
         </Container>
     )
 };
