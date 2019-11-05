a65d4c6c97d1c4375ecf2fea0789a787f4f0045d
yOoMarvin <marvin.messenzehl@gmail.com>
2019-11-05 19:33:36 +0100

    prettier + global css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './components/theme/index';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
